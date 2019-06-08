2c1dc25cac4e7a3fcfcdcc24f11e71635f59597e
DemeritCowboy <demeritcowboy@hotmail.com>
2019-06-08 16:14:14 -0400

    Slightly more configurable. Fix empty cursor bug.

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # github-periodic-summary
-Get a periodic digest of pull requests a bit different from what github offers
+Get a periodic digest of pull requests a bit different from what github offers.
 
 I haven't figured out yet exactly what I want and the script is pretty hokey. Will update here when it's useful.
+
+## Installation
+1. Copy github-summary.cfg.sample to github-summary.cfg
+2. If you don't already have an api token, get one from your github profile settings under developer settings. It needs "repo" access.
+3. Fill github-summary.cfg.
+4. Copy github-summary-lastcursor.cfg to the folder you chose for GITHUB\_SUMMARY\_PATH in github-summary.cfg.
+   * This needs to be seeded. TODO. Can do manually by running a query in the explorer (https://developer.github.com/v4/explorer) like this:  
+   query {
+     repository(owner:"civicrm", name:"civicrm-core") {
+       pullRequests(first:1, orderBy:{field:UPDATED_AT, direction:DESC}) {
+         edges {
+           node {
+             number
+           }
+           cursor
+         }
+       }
+     }
+   }
+   * The output will contain a value for "cursor" that you can put in the github-summary-lastcursor.cfg as a seed value.
+5. Set up a cron script. At the moment it needs to include a cd to the path of the script (TODO: Use $HOME/.github-summary or something as a fixed location for the .cfg file). This example would run every day at 08:00:
+   * `0 8 * * * jsmith cd /path/to/folder && /path/to/folder/github-summary.sh`
--- github-summary.sh
+++ github-summary.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
+# For this to work the cron script has to cd to our folder first.
 . ./github-summary.cfg
-. ./github-summary-lastcursor.cfg
+. $GITHUB_SUMMARY_PATH/github-summary-lastcursor.cfg
 
 # format our query in the weirdo way that the api wants
 read -r -d '' query << ENDQUERY
 {
-  "query": "query { repository(owner:\"civicrm\", name:\"civicrm-core\") { pullRequests(after:\"$GITHUB_SUMMARY_LASTCURSOR\", first:100, orderBy:{field:UPDATED_AT, direction:ASC}) { edges { node { number title updatedAt closed bodyText } cursor } } } }"
+  "query": "query { repository(owner:\"$GITHUB_REPO_OWNER\", name:\"$GITHUB_REPO_NAME\") { pullRequests(after:\"$GITHUB_SUMMARY_LASTCURSOR\", first:100, orderBy:{field:UPDATED_AT, direction:ASC}) { edges { node { number title updatedAt closed bodyText } cursor } } } }"
 }
 ENDQUERY
 
 # Run query and store results in a file (the quoting weirdness is weird enough below, so we use a file to at least avoid dealting with single quotes that are in the returned result).
-curl -s -H "Authorization: bearer $GITHUB_API_TOKEN" -H "Content-type: application/json" -H "Accept: application/json" -X POST -d "$query" https://api.github.com/graphql > github-summary.tmp
+curl -s -H "Authorization: bearer $GITHUB_API_TOKEN" -H "Content-type: application/json" -H "Accept: application/json" -X POST -d "$query" https://api.github.com/graphql > $GITHUB_SUMMARY_PATH/github-summary.tmp
 
 # bash doesn't do json, so we call out to php
 read -r -d '' phpparam << ENDPHP
-\$js = file_get_contents('github-summary.tmp');
+\$js = file_get_contents('$GITHUB_SUMMARY_PATH/github-summary.tmp');
 \$results = json_decode(\$js);
+\$cursor = NULL;
 foreach(\$results->data->repository->pullRequests->edges as \$r) {
   echo "{\$r->node->title}\\n";
   echo "https://github.com/civicrm/civicrm-core/pull/{\$r->node->number}\\n";
@@ -25,7 +27,9 @@ foreach(\$results->data->repository->pullRequests->edges as \$r) {
   echo "{\$r->node->bodyText}\\n\\n========================\\n\\n";
   \$cursor = \$r->cursor;
 }
-file_put_contents('github-summary-lastcursor.cfg', "GITHUB_SUMMARY_LASTCURSOR=\$cursor");
+if (\$cursor) {
+  file_put_contents('$GITHUB_SUMMARY_PATH/github-summary-lastcursor.cfg', "GITHUB_SUMMARY_LASTCURSOR=\$cursor");
+}
 ENDPHP
 
 msg=$( php -r "$phpparam" );
@@ -33,4 +37,4 @@ mail -s "Github Summary" -S "from=$GITHUB_SUMMARY_EMAIL" $GITHUB_SUMMARY_EMAIL <
 $msg
 ENDMSG
 
-rm -f github-summary.tmp
+rm -f $GITHUB_SUMMARY_PATH/github-summary.tmp
